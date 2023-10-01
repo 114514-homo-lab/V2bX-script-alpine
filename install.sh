f6eb2092e4c78dd381d6771adaeb6a8a362d2f58
114514-homo-lab <125758198+114514-homo-lab@users.noreply.github.com>
2023-10-01 09:55:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -205,7 +205,7 @@ install_V2bX() {
     if [[ $first_install == true ]]; then
         read -rp "检测到你为第一次安装V2bX,是否自动直接生成配置文件？(y/n): " if_generate
         if [[ $if_generate == [Yy] ]]; then
-            curl -o ./initconfig.sh -Ls https://raw.githubusercontent.com/InazumaV/V2bX-script/master/initconfig.sh
+            curl -o ./initconfig.sh -Ls https://raw.githubusercontent.com/114514-homo-lab/V2bX-script/master/initconfig.sh
             source initconfig.sh
             rm initconfig.sh -f
             generate_config_file
